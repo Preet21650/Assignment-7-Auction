7236925608e9fdaa8635381e3004df355aee80a0
Preetmohan Manchanda <preetmohansm@outlook.com>
2021-01-26 13:58:18 +0700

    Add project files.

---
+++ Assignment 7 Auction/Item.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_7_Auction
+{
+    class Item
+    {
+        private string des; //description of item 
+        private double RPrice;//reserve price
+        private double HBid; // highest bid
+        private int ID;
+        int BidderID = 0;
+
+        public Item(string des,double RPrice)
+        {
+            this.des = des;
+            this.RPrice = RPrice;
+        }
+        public string GetDes()
+        {
+            return des;
+        }
+        public double GetRPrice()
+        {
+            return RPrice;
+        }
+        public double GetHBid()
+        {
+            return HBid;
+        }
+        public int GetID()
+        {
+            return ID;
+        }
+
+        public bool SetHighestBid(double newBid,int ID)
+        {
+            if (newBid > HBid)
+            {
+                HBid = newBid;
+                ID = BidderID;
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+
+        public void SetBidderID(int ID)
+        {
+            BidderID = ID;
+        }
+    }
+}
---
+++ Assignment 7 Auction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Assignment_7_Auction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item[] Items = new Item[5];
+            Items[0] = new Item("Iphone", 1000);
+            Items[1] = new Item("Ipad", 1500);
+            Items[2] = new Item("Airpods", 7000);
+            Items[3] = new Item("Headphones", 500);
+            Items[4] = new Item("Speakers", 200);
+
+            for (int i = 0; i < Items.Length; i++)
+            {
+                Console.WriteLine(Items[i].GetDes());
+            }
+
+            
+
+        }
+    }
+}
